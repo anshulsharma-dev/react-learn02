21ced6817011fec587d00449c90c4d465fadca0a
Your Name <sharmaanshul9796@gmail.com>
2023-11-22 14:35:26 +0530

    shadow

---
+++ 02lifeCycleHooks/02lifeCycleHooks/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Components from './Components'
+import { ReactDOM } from 'react'
+
+
+function App() {
+  
+
+  return (
+   <>
+   <h1 className='bg primary-500'> Hello</h1>
+   </> 
+)
+}
+
+export default App
